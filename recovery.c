774d8a368c9a1d0738889985d7c258c959a3c38b
Vijaykumar Badiger <vbadig@codeaurora.org>
2017-06-16 11:14:22 -0700

    Cleaning up default update package path
    Cleaning up default update package path. It will be overwritten
    if user gives an updated path or filename.
    
    Change-Id: Ib1b7302aedab2c8061438557a0da533d4eab4c2d
    Signed-off-by: Vijaykumar Badiger <vbadig@codeaurora.org>

--- recovery.c
+++ recovery.c
@@ -692,9 +692,7 @@ main(int argc, char **argv) {
 
     int previous_runs = 0;
     const char *send_intent = NULL;
-    char *update_package = NULL;
-    update_package = (char*)malloc(strlen("/data/update.zip")+1);
-    sprintf(update_package, "%s", "/data/update.zip");
+    char *update_package = "/data/update.zip";
     int wipe_data = 0, wipe_cache = 0;
 
     int arg;
@@ -764,7 +762,6 @@ main(int argc, char **argv) {
 
     // Otherwise, get ready to boot the main system...
     finish_recovery(send_intent);
-    free(update_package);
     ui_print("Rebooting...\n");
     sync();
     syscall(SYS_reboot, LINUX_REBOOT_MAGIC1, LINUX_REBOOT_MAGIC2, LINUX_REBOOT_CMD_RESTART, NULL);
