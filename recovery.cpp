27f02ff455e1f43bf0e668f5eaee95de5717167a
shrkum <quic_shrkum@quicinc.com>
2024-04-13 13:03:19 +0530

    OTA: Provides the option to use update_binay from device
    Change-Id: I8213b6a5f202ae7a56a2585ba1a868a3d4fa7b5a
    (cherry picked from commit 6c567599877fbad50c9d6f3e35fde5647779d6e9)

--- recovery.cpp
+++ recovery.cpp
@@ -123,6 +123,7 @@ static const struct option OPTIONS[] = {
   { "shutdown_after", no_argument, NULL, 'p' },
   { "reason", required_argument, NULL, 'r' },
   { "security", no_argument, NULL, 'e'},
+  { "update_binary_from_device", no_argument, NULL, 'b'},
   { "wipe_ab", no_argument, NULL, 0 },
   { "wipe_package_size", required_argument, NULL, 0 },
   { NULL, 0, NULL, 0 },
@@ -184,6 +185,7 @@ static const char* locale = "en_US";
 char* stage = NULL;
 char* reason = NULL;
 bool modified_flash = false;
+bool update_binary_from_device = false;
 static bool has_cache = false;
 static char* ota_status = NULL;
 /*
@@ -2002,6 +2004,7 @@ int main(int argc, char **argv) {
     bool should_wipe_data = false;
     bool should_wipe_cache = false;
     bool should_wipe_ab = false;
+    update_binary_from_device = false;
     size_t wipe_package_size = 0;
     bool show_text = false;
     bool sideload = false;
@@ -2023,6 +2026,7 @@ int main(int argc, char **argv) {
         case 'w': should_wipe_data = true; break;
         case 'c': should_wipe_cache = true; break;
         case 't': show_text = true; break;
+        case 'b': update_binary_from_device = true; break;
         case 's': sideload = true; break;
         case 'a': sideload = true; sideload_auto_reboot = true; break;
         case 'x': just_exit = true; break;
