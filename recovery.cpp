42162a45e4bc0c986bda5b1530005d444a69d956
shrkum <quic_shrkum@quicinc.com>
2024-12-12 12:44:53 +0530

    OTA: /cache should be mounted before creating recovery folder
    As after corruption recovery of cache partition platform team is not mounting and recovery-binary is creating folder without checking if /cache is mounted or not
    
    Change-Id: If650576943d73daba5b310a5513fabd2b16c6bbb

--- recovery.cpp
+++ recovery.cpp
@@ -2004,6 +2004,7 @@ int main(int argc, char **argv) {
       printf(" recovery folder exist \n");
     }else{
       printf(" recovery folder doesnt exist, create folder  \n");
+      ensure_path_mounted("/cache");
       umask(000);
       mkdir(CACHE_LOG_DIR, 0777);
     }
